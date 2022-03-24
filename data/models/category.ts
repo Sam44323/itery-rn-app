fa9fcd8b1f024de6524607ed3019c47d428cdd0f
Suranjan <suranjanbiswas2002@gmail.com>
2022-03-25 00:43:18 +0530

    add: added demo data with models

---
+++ data/models/category.ts
@@ -0,0 +1,12 @@
+class Category {
+  id: any;
+  title: string;
+  color: string;
+  constructor(id: any, title: string, color: string) {
+    this.id = id;
+    this.title = title;
+    this.color = color;
+  }
+}
+
+export default Category;
