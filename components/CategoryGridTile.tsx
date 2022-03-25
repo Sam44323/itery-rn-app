3a844af7ca873afea625d6ccf44f8a10588e4f73
Suranjan <suranjanbiswas2002@gmail.com>
2022-03-25 13:24:06 +0530

    add: added screens for categories and title

---
+++ components/CategoryGridTile.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Pressable, View, Text } from "react-native";
+
+interface CategoryGridTileProps {
+  title: string;
+  color: string;
+}
+
+const CategoryGridTile: React.FC<CategoryGridTileProps> = ({
+  title,
+  color,
+}) => {
+  return (
+    <View>
+      <Pressable>
+        <View>
+          <Text>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+
+export default CategoryGridTile;
--- screens/CategoriesScreens.tsx
+++ screens/CategoriesScreens.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { FlatList } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
 
 const CategoriesScreens: React.FC = () => {
-  return <></>;
+  const renderCategoryItem = (itemData: any) => {
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+      />
+    );
+  };
+
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+    />
+  );
 };
 
 export default CategoriesScreens;
