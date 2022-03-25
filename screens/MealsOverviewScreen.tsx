323f36f4923ef072b363b68691f43b255c01a6d4
Suranjan <suranjanbiswas2002@gmail.com>
2022-03-25 17:12:09 +0530

    add: added embedded navigation_title

--- screens/MealsOverviewScreen.tsx
+++ screens/MealsOverviewScreen.tsx
@@ -1,12 +1,20 @@
-import { MEALS } from "../data/dummy-data";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 import React from "react";
 import { View, StyleSheet, FlatList } from "react-native";
 import MealItem from "../components/MealItem";
 
-const MealsOverviewScreen: React.FC = ({ route }: any) => {
+const MealsOverviewScreen: React.FC = ({ route, navigation }: any) => {
   const displayMeals = MEALS.filter(
     (mealItem) => mealItem.categoryIds.indexOf(route.params.categoryId) >= 0
   );
+
+  React.useEffect(() => {
+    // example of setting the options for the screen from inside
+    navigation.setOptions({
+      title: CATEGORIES.find((cat) => cat.id === route.params.categoryId)!
+        .title,
+    });
+  }, []);
 
   const renderMealItem = (itemData: any) => {
     return <MealItem {...itemData.item} />;
