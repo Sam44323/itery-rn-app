4719599e29661d50b5a23dde530efeed9596a04c
Suranjan <suranjanbiswas2002@gmail.com>
2022-03-25 14:52:58 +0530

    add: added component for custom_meal item data

--- screens/MealsOverviewScreen.tsx
+++ screens/MealsOverviewScreen.tsx
@@ -1,11 +1,24 @@
 import { MEALS } from "../data/dummy-data";
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet, FlatList } from "react-native";
+import MealItem from "../components/MealItem";
 
 const MealsOverviewScreen: React.FC = ({ route }: any) => {
+  const displayMeals = MEALS.filter(
+    (mealItem) => mealItem.categoryIds.indexOf(route.params.categoryId) >= 0
+  );
+
+  const renderMealItem = (itemData: any) => {
+    return <MealItem title={itemData.item.title} />;
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Meals overview screen! - {route.params.categoryId}</Text>
+      <FlatList
+        data={displayMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
